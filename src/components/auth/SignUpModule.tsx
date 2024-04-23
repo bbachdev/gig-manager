ba11c5e306680e5ab805589c891ee4fa440e9a37
Barry Bach <bbach7139@gmail.com>
2024-04-23 10:20:37 -0500

    Add password requirements to Sign Up

--- src/components/auth/SignUpModule.tsx
+++ src/components/auth/SignUpModule.tsx
@@ -6,10 +6,36 @@ import { useForm } from "react-hook-form"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
 import { Input } from '../ui/input';
 
+import { IoIosCloseCircle } from "react-icons/io";
+import { IoIosCheckmarkCircle } from "react-icons/io";
+
 const signUpSchema = z.object({
   email: z.string().email(),
   password: z.string(),
 })
+
+const passwordRequirements = [
+  {
+    condition: (pass: string) => pass.length >= 8,
+    text: `At least 8 characters`,
+  },
+  {
+    condition: (pass: string) => /[a-z]/.test(pass),
+    text: `At least one lowercase letter`,
+  },
+  {
+    condition: (pass: string) => /[A-Z]/.test(pass),
+    text: `At least one uppercase letter`,
+  },
+  {
+    condition: (pass: string) => /[0-9]/.test(pass),
+    text: `At least one number`,
+  },
+  {
+    condition: (pass: string) => /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(pass),
+    text: `At least one special character`,
+  }
+]
 
 export default function SignUpModule() {
   const form = useForm<z.infer<typeof signUpSchema>>({
@@ -28,7 +54,7 @@ export default function SignUpModule() {
     <>
       <Card className={`w-1/3`}>
         <CardHeader>
-          <h1 className={`text-3xl font-bold text-center`}>Sign In</h1>
+          <h1 className={`text-3xl font-bold text-center`}>Sign Up</h1>
         </CardHeader>
         <CardContent>
           <Form {...form}>
@@ -49,6 +75,17 @@ export default function SignUpModule() {
                     <Input type="password" {...field} />
                   </FormControl>
                   <FormMessage/>
+                  <div className={`text-sm`}>
+                    <span className={`font-bold`}>{`Passwords must contain:`}</span>
+                    <ul className={`text-sm mt-2`}>
+                      {passwordRequirements.map(({ condition, text }, i) => (
+                        <li key={i}>
+                          {condition(field.value) ? <IoIosCheckmarkCircle className={`text-green-500 inline`} /> : <IoIosCloseCircle className={`text-red-500 inline`} />}
+                          <span className={`ml-1`}>{text}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 </FormItem>
               )} />
               <button type="submit" className={`bg-blue-500 hover:bg-blue-500/90 text-white rounded-md p-2 w-full`}>Sign Up</button>
